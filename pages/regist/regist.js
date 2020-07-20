36b64ab34d98d7ce6110d0eb6c3bd20324a817cf
chen-yh <529352831@qq.com>
2020-07-20 13:47:43 +0800

    app.glogbal

--- pages/regist/regist.js
+++ pages/regist/regist.js
@@ -16,15 +16,64 @@ Page({
     
   },
   getUserInfo: function(e){
-   console.log(e)         
-          wx.switchTab({          
-            url: '../index/index',
-            success: (res)=>{
-              console.log(res)
-            }
-          })
-      
-    
+    console.log(e.detail.userInfo)
+   app.globalData.userInfo = e.detail.userInfo
+   wx.login({
+    success (res) {
+      console.log('rrr', res)
+      // let token1 = res.code
+      if (res.code) {
+        //发起网络请求
+        wx.request({
+          method:'POST',
+          header: {
+            'content-type': 'application/x-www-form-urlencoded' // 默认值
+          },
+          url: 'http://192.168.33.22/weixin/getopenid',
+          data: {
+            code: res.code
+          },
+          success: (datas) => {
+            console.log('tttt', datas)  
+            wx.setStorageSync('token', datas.data.code)
+          },
+          fail(res3) {
+            console.log("获取用户信息失败", res)
+          }    
+        })
+      }
+    }
+      })
+      wx.request({
+              method: 'POST',
+              header: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'token': wx.getStorageInfoSync('token')
+              },
+              url: 'http://192.168.33.22/weixin/gettoken',
+              data: {
+                city: e.detail.userInfo.city,
+                country: e.detail.userInfo.country,
+                createdate: '',
+                headimgurl: e.detail.userInfo.avatarUrl,
+                iphone: '1579655656',
+                nickname: e.detail.userInfo.nickName,
+                province: e.detail.userInfo.province
+              },
+              success: (data2)=>{
+                console.log(data2)
+                wx.setStorageSync('token', data2.data.code)
+                // wx.switchTab({          
+                //   url: '../index/index',
+                //   success: (res)=>{
+                //     console.log(res)
+                //   }
+                // })
+              }
+            })
+
+            //
+            
   },
 
   /**
