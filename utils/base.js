69e6bbe73205b2d9afe283190d732aea05b1cd8b
smarkers <983183973@qq.com>
2020-08-13 15:36:18 +0800

    revise调试1

--- utils/base.js
+++ utils/base.js
@@ -1,6 +1,6 @@
 class Base {
   constructor(){
-    this.baseRequestUrl = "http://192.168.32.93/"
+    this.baseRequestUrl = "http://192.168.32.177/"
   }
   //http://192.168.16.68/swagger-ui.html#/
   request(params){
